b403485a45157c14d9ad1c4821ae110e54dc4143
shaolin-kongfu <fanxian546@gmail.com>
2020-06-14 19:00:49 +0800

    修改

--- danmuku.py
+++ danmuku.py
@@ -2,14 +2,14 @@ import requests
 import xml2ass
 aid = input('请输入aid:')
 r = requests.get("https://www.bilibili.com/widget/getPageList?aid=" + aid)
-print(eval(r.text))
+#print(eval(r.text))
 for i in eval(r.text):
     dict = i
 cid = dict['cid']
-print(cid)
+#print(cid)
 danmuku = requests.get('https://comment.bilibili.com/' + str(cid) + '.xml')
 danmuku.encoding = 'utf-8'
-print(type(danmuku.text))
+#print(type(danmuku.text))
 with open('./danmu.xml', 'w',encoding = 'utf-8') as file:
     file.write(danmuku.text)
 file.close()
