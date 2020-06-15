40a06affa4d6c425b605128ea6481329a1de69da
shaolin-kongfu <fanxian546@gmail.com>
2020-06-15 20:51:03 +0800

    更新支持多P视频弹幕下载

--- danmuku.py
+++ danmuku.py
@@ -1,5 +1,7 @@
 import requests
 import xml2ass
+import os
+# 链接格式：https://www.bilibili.com/video/BV******* (后面不能有/)
 url = input('输入视频链接：')
 bv = url[len('https://www.bilibili.com/video/'):]
 headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
@@ -8,16 +10,24 @@ dict_aid = eval(a.text)
 aid = dict_aid['data']['aid']
 # print(aid)
 r = requests.get("https://www.bilibili.com/widget/getPageList?aid=" + str(aid),headers = headers)
-#print(eval(r.text))
+# print(eval(r.text))
+cid = []
 for i in eval(r.text):
     dict = i
-cid = dict['cid']
-#print(cid)
-danmuku = requests.get('https://comment.bilibili.com/' + str(cid) + '.xml',headers = headers)
-danmuku.encoding = 'utf-8'
-#print(type(danmuku.text))
-with open('./danmu.xml', 'w',encoding = 'utf-8') as file:
-    file.write(danmuku.text)
-file.close()
-xml2ass.Danmaku2ASS('danmu.xml','danmu.ass',1920,540)
+    every_cid = dict['cid']
+    cid.append(every_cid)
+# print(len(cid))
+
+# *******多P视频分P下载弹幕************
+for j in range(len(cid)):
+    p_cid = cid[j]
+    # print(p_cid)
+    danmuku = requests.get('https://comment.bilibili.com/' + str(p_cid) + '.xml',headers = headers)
+    danmuku.encoding = 'utf-8'
+    # print(type(danmuku.text))
+    with open('./'+str(j)+'.xml', 'w',encoding = 'utf-8') as file:
+        file.write(danmuku.text)
+    file.close()
+    xml2ass.Danmaku2ASS('./'+str(j)+'.xml','./'+str(j)+'.ass',1920,540)
+    os.remove('./'+str(j)+'.xml')
 
